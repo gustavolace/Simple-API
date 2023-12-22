6b52ebfc68930696ec6a9cec321e93de79afdf72
gustavolace <lsbgustavo@gmail.com>
2023-12-22 20:56:58 -0300

    Adicionar arquivos de projeto.

---
+++ Simple API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Simple_API.Models;
+
+namespace Simple_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UserModel>> FindUsers()
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ Simple API/Data/TaskDataController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Simple_API.Models;
+
+namespace Simple_API.Data
+{
+    public class TaskDataController : DbContext
+    {
+        public TaskDataController(DbContextOptions<TaskDataController> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
